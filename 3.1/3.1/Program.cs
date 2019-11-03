4c6cef490dfc10abd62c71771bb2c4e83f8703b5
EliseevVadim <eliseevv02@mail.ru>
2019-11-03 19:36:18 +0300

    T1

---
+++ 3.1/3.1/Program.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rand = new Random();
+            int N = int.Parse(Console.ReadLine());
+            int[] array = new int[N];
+            int a = 0;
+            int c = 10;
+          int  b= N;
+            for (int i=0; i<array.Length; i++)
+            {
+                array[i] = rand.Next(-30, 45);
+            }
+            while (b > 0)
+            {
+                for (int i = a; i < c; i++)
+                {
+                    Console.Write(array[i] + " ");
+                }
+                Console.WriteLine();
+                b -= 10;
+               a += 10;
+               c += 10;
+            }
+            Console.WriteLine("________________________________________________________________________________");
+            for (int i=array.Length-1; i>=0; i--)
+            {
+                if (array[i]>=0)
+                {
+                    Console.Write(array[i] + " ");
+                }
+            }
+
+            Console.ReadKey();
+
+        }
+    }
+}
