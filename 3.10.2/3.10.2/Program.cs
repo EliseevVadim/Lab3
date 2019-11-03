cf3405f1994219c7c5ab40a194f741cd5e0f6186
EliseevVadim <eliseevv02@mail.ru>
2019-11-03 19:03:21 +0300

    V10T2

---
+++ 3.10.2/3.10.2/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3._10._2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int N = int.Parse(Console.ReadLine());
+            int[] array = new int[N];
+            int counter = 0;
+            int sum=0;
+            for (int i=0; i<array.Length; i++)
+            {
+                array[i] = int.Parse(Console.ReadLine());
+                sum += array[i];
+            }
+            double middle_arithmetical = sum / array.Length;
+            for (int i=0; i<array.Length;i++)
+            {
+                if (array[i]>middle_arithmetical)
+                {
+                    counter++;
+                }
+            }
+            Console.WriteLine(counter);
+            
+            Console.ReadKey();
+
+        }
+    }
+}
