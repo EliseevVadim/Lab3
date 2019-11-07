65afecff01a32b4fc913259120a0c2b6978c61ca
EliseevVadim <eliseevv02@mail.ru>
2019-11-07 21:08:36 +0300

    T3(1)

---
+++ 3.3/3.3/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3._3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int N = int.Parse(Console.ReadLine());
+            int[] array = new int[N];
+            for (int i=0; i<array.Length; i++)
+            {
+                array[i] = int.Parse(Console.ReadLine());
+            }
+            int k = int.Parse(Console.ReadLine());
+            int[] newArray = new int[N - k];
+            for (int i=0;i<newArray.Length; i++)
+            {
+                newArray[i] = array[i + k];
+                Console.Write(newArray[i] + " ");
+            }
+            Console.ReadKey();
+        }
+    }
+}
