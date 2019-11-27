39dfe9deaabed8e2cf25d323efc0f05a4c2b2f28
EliseevVadim <eliseevv02@mail.ru>
2019-11-27 22:48:37 +0300

    V10T1

---
+++ 3.10.1/3.10.1/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace _3._10._1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] array = new int[9, 9];
+            int num = 1, counter = 0;
+            bool flag = true;
+            for (int i = 0; i < array.GetLength(0); i++)
+            {
+                for (int j = 0; j < array.GetLength(1); j++)
+                {
+                    if (j < counter || array.GetLength(1)-1 - counter < j)
+                    {
+                        array[j, i] = num;
+                        num++;
+                    }
+                }
+                if (flag && counter > 3)
+                {
+                    flag = false;
+                }
+                if (flag)
+                {
+                    counter++;
+                }
+                else
+                {
+                    counter--;
+                }
+            }
+            for (int i = 0; i < array.GetLength(0); i++)
+            {
+                for (int j = 0; j < array.GetLength(1); j++)
+                {
+                    Console.Write(array[i, j] + " ");
+                }
+                Console.WriteLine();
+            }
+        }
+    }
+}
